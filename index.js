296382a6f347085ee5b73c51c2be482faad35354
Alan Escarcha <alanescarcha11@gmail.com>
2021-10-03 04:18:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+var port = process.env.PORT || 8080;
+
+async function getData(username) {
+    try {
+        const response = await axios.get(`https://playerdb.co/api/player/minecraft/${username}`);
+        return response.data.data.player;
+    } catch (error) {
+        return;
+    }
+}
+
+app.get('/mojang/v1/:user', async function (req, res) {
+    try {
+        const player = await getData(req.params.user);
+        const raw_id = await player.raw_id;
+        await res.status(200).json(
+            {
+                mojang: {
+                    username: player.username,
+                    uuid: player.id,
+                    raw_uuid: raw_id,
+                    username_history: player.meta.name_history
+                },
+                skin: {
+                    avatar: `https://crafatar.com/avatars/${raw_id}?overlay=true`,
+                    head_render: `https://crafatar.com/renders/head/${raw_id}?overlay=true`,
+                    body_render: `https://crafatar.com/renders/body${raw_id}?overlay=true`,
+                    skin: `https://crafatar.com/skins/${raw_id}`,
+                    cape: `https://crafatar.com/capes/${raw_id}`
+                }
+            }
+        );
+    } catch (error){
+        res.status(500).send('Error 500');
+    }
+});
+
+app.listen(port, ()=>{
+    console.info(`Listening API on port: ${port}`)
+});
