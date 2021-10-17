47cd24d1d88c4d5253d0776673f3faa17433fafc
Alan Escarcha <alanescarcha11@gmail.com>
2021-10-17 16:43:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const v1 = require("./mojang/v1/api");
 
 app.use(express.json({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "mctools.pro"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use("/mojang/v1", v1);
 
 const PORT = process.env.PORT || 8080;
